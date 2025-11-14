7a466bbce352cbc8181a4e2618c8bef12480b2d1
Nikolas Bradshaw <nikolas.bradshaw@sapphirefountains.com>
2025-11-14 14:19:20 -0700

    Update task.py

--- task_enhancements/task/task.py
+++ task_enhancements/task/task.py
@@ -40,39 +40,56 @@ class Task(NestedSet):
 
 @frappe.whitelist()
 def get_child_tasks_html(task_name):
+    """
+    Gets all descendant tasks and builds a nested HTML structure for them.
+    """
     task = frappe.get_doc("Task", task_name)
+    # Using get_descendants() is the correct approach for a NestedSet model
     descendants = task.get_descendants()
     
     if not descendants:
         return ""
 
+    # A dictionary to hold tasks by their name for easy lookup
     task_map = {d.name: d for d in descendants}
+    # Initialize a children list for each task
     for d in descendants:
         d.children = []
 
+    # This list will hold the top-level tasks in the hierarchy below the main task
     tree = []
     for d in descendants:
+        # If the parent is the main task, it's a direct child
         if d.parent_task == task_name:
             tree.append(d)
+        # If the parent is in our map, it's a nested child
         elif d.parent_task in task_map:
             task_map[d.parent_task].children.append(d)
 
     return _build_task_tree_html(tree)
 
 def _build_task_tree_html(tasks):
+    """
+    Recursively builds an HTML unordered list for a list of tasks and their children.
+    """
     if not tasks:
         return ""
 
+    # Start the list
     html = "<ul>"
     for task in tasks:
         html += "<li>"
+        # Add a toggle icon if the task has children
         if task.children:
-            html += '<i class="fa fa-plus-square toggle-child-tasks"></i> '
+            html += '<i class="fa fa-minus-square toggle-child-tasks" style="cursor: pointer;"></i> '
         else:
+            # Use a different icon for tasks without children for visual clarity
             html += '<i class="fa fa-square-o"></i> '
         
+        # Add the link to the task
         html += f'<a href="/app/task/{task.name}">{task.subject}</a>'
         
+        # If there are children, recursively call this function to build the nested list
         if task.children:
             html += _build_task_tree_html(task.children)
             
